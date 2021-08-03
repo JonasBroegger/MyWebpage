44c42b03697f87826c3413f7fb4d0b1b09587ad0
JonasBroegger <jonasbroegger@gmail.com>
2021-08-03 22:09:07 +0200

    Implemented contact-form

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,10 +1,15 @@
 import React, { PropsWithChildren } from "react";
 
 interface IButtonProps {
-    onClick: (event: React.MouseEvent) => void;
-    type: string;
+  onClick?: (event: React.MouseEvent) => void;
+  type: "button" | "submit" | "reset" | undefined;
+  disabled: boolean;
 }
 
-export default function Button(props: PropsWithChildren<IButtonProps>){
-    return <button onClick={props.onClick}>{props.children}</button>
+export default function Button(props: PropsWithChildren<IButtonProps>) {
+  return (
+    <button onClick={props.onClick} type={props.type} disabled={props.disabled}>
+      {props.children}
+    </button>
+  );
 }
--- src/Components/Contact/ContactForm.tsx
+++ src/Components/Contact/ContactForm.tsx
@@ -1,22 +1,111 @@
-import Button from '../Button'
-import './ContactForm.scss'
+import { ChangeEvent, FormEvent, useRef, useState } from "react";
+import axios from "axios";
+import Button from "../Button";
+import "./ContactForm.scss";
 
-export default function ContactForm(){
+type FormState = {
+  email: string;
+  name: string;
+  message: string;
+};
 
-    function handleSubmit(){
-        console.log("Submitted")
+type ServiceMessage = {
+  text: string;
+};
+
+export default function ContactForm() {
+  const formId = "vflkGnEh";
+  const formSparkUrl = `https://submit-form.com/${formId}`;
+
+  const initialFormState = {
+    email: "",
+    name: "",
+    message: "",
+  };
+
+  const [formState, setFormState] = useState<FormState>(initialFormState);
+  const [submitting, setSubmitting] = useState<boolean>(false);
+  const [message, setMessage] = useState<ServiceMessage>();
+
+  async function handleSubmit(event: FormEvent):Promise<void> {
+    event.preventDefault();
+    setSubmitting(true);
+    await postSubmission();
+    setSubmitting(false);
+  }
+
+  const postSubmission = async () => {
+    const payload = {
+      ...formState,
+    };
+
+    try {
+      const result = await axios.post(formSparkUrl, payload);
+      console.log(result);
+      setMessage({
+        text: "Thanks, I will be in touch shortly.",
+      });
+      setFormState(initialFormState);
+    } catch (error) {
+      console.log(error);
+      setMessage({
+        text: "Sorry, there was a problem. Please try again later.",
+      });
     }
+  };
 
-    return <div className="MainContact">
-        <div>
-            <h1>Contact me</h1>
-        </div>
-        <form>
-            <input placeholder="Name" type= "text" required></input>
-            <input placeholder="Your Email" type= "email" required></input>
-            <br/>
-            <textarea placeholder ="Your Message" rows={5}></textarea>
-            <Button type="submit" onClick={handleSubmit}>Submit</Button>
-        </form>
+  const updateFormControl = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { id, value } = event.target;
+    const key = id as keyof FormState;
+    const updatedFormState = { ...formState };
+    updatedFormState[key] = value;
+    setFormState(updatedFormState);
+  };
+
+  return (
+    <div className="MainContact">
+      <div>
+        <h1>Contact me</h1>
+      </div>
+      {message && ( 
+      <div className="ServiceMessage">
+          {message.text}
+      </div>)}
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          onChange={updateFormControl}
+          type="text"
+          id="name"
+          value={formState?.name}
+          required
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          onChange={updateFormControl}
+          type="email"
+          id="email"
+          value={formState?.email}
+          required
+        />
+
+        <label htmlFor="message">Message</label>
+        <textarea
+          onChange={updateFormControl}
+          id="message"
+          value={formState?.message}
+          required
+          rows={5}
+        ></textarea>
+
+        <Button type="submit" disabled={submitting}>
+          {submitting ? "Submitting..." : "Submit"}
+        </Button>
+      </form>
     </div>
+  );
 }
